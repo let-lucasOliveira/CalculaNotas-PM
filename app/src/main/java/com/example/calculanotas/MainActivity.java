47600985796903828a29ae36cce8fd138fc1fea9
let-lucasOliveira <lucas.nro1000@gmail.com>
2025-08-21 19:58:44 -0300

    ✨ Add buttons and fields to calculate the mounth average

--- app/src/main/java/com/example/calculanotas/MainActivity.java
+++ app/src/main/java/com/example/calculanotas/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.calculanotas;
 
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
